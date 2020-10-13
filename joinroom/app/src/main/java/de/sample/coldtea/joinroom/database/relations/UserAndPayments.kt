ea4531865685525120e882c32356c916e16ce192
Yasar Naci Gündüz <yasarnacigunduz@gmail.com>
2020-10-13 15:22:52 +0200

    added sql equivalent

--- joinroom/app/src/main/java/de/sample/coldtea/joinroom/database/relations/UserAndPayments.kt
+++ joinroom/app/src/main/java/de/sample/coldtea/joinroom/database/relations/UserAndPayments.kt
@@ -5,7 +5,7 @@ import androidx.room.Relation
 import de.sample.coldtea.joinroom.database.entities.AddressEntity
 import de.sample.coldtea.joinroom.database.entities.PaymentEntity
 import de.sample.coldtea.joinroom.database.entities.UserEntity
-
+//SELECT * FROM UserEntity as U LEFT JOIN PaymentEntity as P ON (U.user_id = P.fk_user_id)
 data class UserAndPayments(
     @Embedded val user: UserEntity,
     @Relation(
