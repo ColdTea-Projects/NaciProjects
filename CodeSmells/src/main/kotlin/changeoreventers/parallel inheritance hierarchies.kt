c484e6ef6522d55a5835406d9511c3740a5ca2e9
Yasar Naci Gündüz <yasarnacigunduz@gmail.com>
2021-01-23 12:55:47 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ CodeSmells/src/main/kotlin/changeoreventers/parallel inheritance hierarchies.kt
@@ -0,0 +1,78 @@
+package changeoreventers
+
+interface SmartPhone{
+    fun ring()
+}
+
+open class Android():SmartPhone{
+    override fun ring() {
+        TODO("Not yet implemented")
+    }
+}
+
+class IPhone():SmartPhone{
+    override fun ring() {
+        TODO("Not yet implemented")
+    }
+
+}
+
+
+/////////////
+
+interface ScreenProtector{
+    var size:Int
+    fun stick()
+}
+
+open class AndroidScreenProtector(override var size: Int = 10) : ScreenProtector{
+    override fun stick() {
+        TODO("Not yet implemented")
+    }
+}
+
+open class IPhoneScreenProtector(override var size: Int = 11) : ScreenProtector{
+    override fun stick() {
+        TODO("Not yet implemented")
+    }
+}
+
+
+/////////Other Androids come in//////
+
+class LG: Android(){}
+class HTC : Android(){}
+
+class LGScreenProtector: AndroidScreenProtector() {
+    init {
+        size = 13
+    }
+}
+class HTCScreenProtector: AndroidScreenProtector(){
+    init {
+        size = 15
+    }}
+
+// Better would be//////
+
+open class BetterAndroid():SmartPhone{
+    protected var screenProtector = AndroidScreenProtector()
+
+    override fun ring() {
+        TODO("Not yet implemented")
+    }
+}
+
+open class BetterLG(): BetterAndroid(){
+    init {
+        screenProtector = AndroidScreenProtector(13)
+    }
+}
+
+open class BetterHTC: BetterAndroid(){
+    init {
+        screenProtector = AndroidScreenProtector(15)
+    }
+}
+
+
